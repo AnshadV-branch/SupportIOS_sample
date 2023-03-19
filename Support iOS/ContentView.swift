a62ba8a2e8e321e37c9695227da8b7b2a62b3eb6
AnshadV-branch <112856579+AnshadV-branch@users.noreply.github.com>
2023-03-19 16:11:37 +0530

    Initial Commit

---
+++ Support iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Support iOS
+//
+//  Created by Anshad V on 19/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Support iOS/Support_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Support_iOSApp.swift
+//  Support iOS
+//
+//  Created by Anshad V on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Support_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
